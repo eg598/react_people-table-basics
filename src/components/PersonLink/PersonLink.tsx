57839b4a7d2ca72f267acfbb6097a0e318eefab0
Yegor <egor598000@gmail.com>
2026-01-28 23:54:11 +0100

    solution

---
+++ src/components/PersonLink/PersonLink.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import { Person } from '../../types';
+import { usePeople } from '../PeopleContext/PeopleContext';
+import { NavLink, useParams } from 'react-router-dom';
+
+type Props = {
+  person: Person;
+};
+
+export const PersonLink: React.FC<Props> = ({ person }) => {
+  const { peopleFromServer } = usePeople();
+  const { selectedPerson } = useParams();
+
+  const validateFather = (name: string): boolean => {
+    const names = [...peopleFromServer].map(p => p.name);
+
+    return names.includes(name);
+  };
+
+  const findPerson = (name: string): Person | undefined => {
+    return peopleFromServer.find(p => p.name === name) || undefined;
+  };
+
+  return (
+    <tr
+      data-cy="person"
+      key={person.name}
+      className={
+        selectedPerson?.replace('#/people/', '') === person.slug
+          ? `has-background-warning`
+          : ''
+      }
+    >
+      <td>
+        <NavLink
+          to={`/people/${person.slug}`}
+          className={person.sex === 'f' ? 'has-text-danger' : ''}
+        >
+          {person.name}
+        </NavLink>
+      </td>
+
+      <td>{person.sex}</td>
+      <td>{person.born}</td>
+      <td>{person.died}</td>
+      <td>
+        {person.motherName ? (
+          validateFather(person.motherName) ? (
+            <NavLink
+              to={`/people/${findPerson(person.motherName)?.slug}`}
+              className="has-text-danger"
+            >
+              {person.motherName}
+            </NavLink>
+          ) : (
+            person.motherName
+          )
+        ) : (
+          '-'
+        )}
+      </td>
+      <td>
+        {person.fatherName ? (
+          validateFather(person.fatherName) ? (
+            <NavLink to={`/people/${findPerson(person.fatherName)?.slug}`}>
+              {person.fatherName}
+            </NavLink>
+          ) : (
+            person.fatherName
+          )
+        ) : (
+          '-'
+        )}
+      </td>
+    </tr>
+  );
+};
